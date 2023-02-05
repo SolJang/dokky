5848574a17d4216e1cd9d73b4120e1102cebd6d9
jangsol <jangsol96@gmail.com>
2023-02-05 13:46:08 +0900

    Initial commit

---
+++ src/main/java/com/study/dokky/DokkyApplication.java
@@ -0,0 +1,13 @@
+package com.study.dokky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DokkyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DokkyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/dokky/DokkyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.dokky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DokkyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
